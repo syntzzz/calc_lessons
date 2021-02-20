b273760b2268eb95aa43eed7002c5ad754fec5cb
syntzzz <b00tl3gg3rs@gmail.com>
2021-02-21 02:52:59 +0800

    main

--- calc_lessons.py
+++ calc_lessons.py
@@ -1,4 +1,4 @@
-print('{:^65}'.format('«Калькулятор расписания уроков»'))
+print('\n{:^65}'.format('«Калькулятор расписания уроков»'))
 
 # Функция длительности урока
 def f_lg():
@@ -6,11 +6,20 @@ def f_lg():
     print('\nДлина урока - 45 минут.')
     while True:
         lg_inp = input('Изменить? [да/нет]\n')
+        lg_inp = lg_inp.lower()
         if lg_inp == 'да':
             lg = input('Длина урока: ')
+            while True:
+                if not lg.isnumeric():
+                    lg = input('Введите цифру. ')
+                else:
+                    break
             break
         elif lg_inp == 'нет':
             lg = 45
+            break
+        elif lg_inp != 'да' or 'нет':
+            f_lg()
             break
     lg = int(lg)
 
@@ -20,11 +29,20 @@ def f_br():
     print('\nБольшая перемена - 15 минут.')
     while True:
         br_inp = input('Изменить? [да/нет]\n')
+        br_inp = br_inp.lower()
         if br_inp == 'да':
             br = input('Длина большой перемены: ')
+            while True:
+                if not br.isnumeric():
+                    br = input('Введите цифру. ')
+                else:
+                    break
             break
         elif br_inp == 'нет':
             br = 15
+            break
+        elif br_inp != 'да' or 'нет':
+            f_br()
             break
     br = int(br)
 
@@ -33,20 +51,18 @@ def calc():
     f_lg()
     f_br()
 
-# Получение часов на ввод и проверка на 24-часовой формат
+    # Получение часов на ввод и проверка на 24-часовой формат
     ch = input('Введите час начала уроков: ')
     while True:
         if not ch.isnumeric():
-            print('Введите цифру. ')
-            ch = input()
+            ch = input('Введите цифру. ')
         elif int(ch) > 24 or int(ch) < 0:
-            print('Неверно задан час. Введите снова: ')
-            ch = input()
+            ch = input('Неверно задан час. Введите снова: ')
         else:
             break
     ch = int(ch)
 
-# Получение минут на ввод и проверка на 24-часовой формат
+    # Получение минут на ввод и проверка на 24-часовой формат
     mn = input('Введите минуты начала уроков: ')
     while True:
         if not mn.isnumeric():
@@ -57,21 +73,23 @@ def calc():
             break
     mn = int(mn)
 
-# Получение кол-ва уроков на ввод
+    # Получение кол-ва уроков на ввод
     kl = (input('Введите количество уроков: '))
     while True:
         if not kl.isnumeric():
             kl = input('Введите цифру. ')
+        elif kl == '0':
+            kl = input('Введите число больше нуля: ')
         else:
             break
     kl = int(kl)
 
-# Вычисление общей длительности перемен
+    # Вычисление общей длительности перемен
     pr = (kl * 5) - 5
     if kl > 3:
         pr += br - 5
 
-# Вычисление общей длительности часов
+    # Вычисление общей длительности часов
     ch = ch + (((kl * lg) + pr) // 60)
     while True:
         if ch > 24:
@@ -79,7 +97,7 @@ def calc():
         else:
             break
 
-# Вычисление общей длительности минут
+    # Вычисление общей длительности минут
     mn = mn + (((kl * 45) + pr) % 60)
     while True:
         if mn >= 60:
@@ -90,7 +108,7 @@ def calc():
     if mn < 10:
         mn = '0' + str(mn)
 
-# Вывод
+    # Вывод
     print('\nУроки закончатся в', str(ch) + ':' + str(mn) + '.')
 
 calc()
@@ -98,6 +116,7 @@ calc()
 while True:
     print('\nВыйти - любая клавиша.\nНачать заново? [да] ')
     flag = input()
+    flag = flag.lower()
     if flag == 'да':
         calc()
     else:
